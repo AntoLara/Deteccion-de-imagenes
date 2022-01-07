e34fd721c896547f36e8b4f51fd34d05df008915
AntoLara <jalch@azc.uam.mx>
2022-01-07 15:11:54 -0600

    Update emotion-detection.py

--- emotion-detection.py
+++ emotion-detection.py
@@ -2,8 +2,8 @@ import cv2
 from deepface import DeepFace
 import numpy as np  #this will be used later in the process
 
-
-imgpath = "/home/hugo/Pictures/capture.jpg"
+imgpath="/home/antonio/Documents/Git/Deteccion-de-imagenes/jose.jpg"
+#imgpath = "/home/hugo/Pictures/capture.jpg"
 
 #analyze = DeepFace.analyze(imgpath,actions=['emotion', 'age', 'gender', 'race'],models={}, enforce_detection=True)
 analyze = DeepFace.analyze(imgpath, actions=['emotion', 'age', 'gender', 'race'], models={}, enforce_detection=False)
